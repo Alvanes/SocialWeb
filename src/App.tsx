21f5bf03bed56404fa4293742345f76eaf9f8a99
Alvanes <yan.spbk@mail.ru>
2021-12-17 15:06:16 +0300

    Social Web

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div className={"grid-conteiner"}>
+        <div className={"header"}>
+            <div>
+                <a target="_blank" href="https://www.codewars.com/users/Alvanes"><img
+                    src="https://www.codewars.com/users/Alvanes/badges/small" alt="codewars badge large"/></a>
+            </div>
+            This is Header
+        </div>
+        <div className={"sidebar"}>
+            <div>
+                <a href="#"><img width={"50px"} src={"https://cdn-icons-png.flaticon.com/512/25/25694.png"}/></a>
+            </div>
+            <div>
+                <a href="#"><img width={"50px"} src={"https://cdn-icons-png.flaticon.com/512/25/25236.png"}/></a>
+            </div>
+            <div>
+                <a href="#"><img width={"50px"} src={"https://cdn-icons-png.flaticon.com/512/25/25319.png"}/></a>
+            </div>
+        </div>
+        <div className={"mainpage"}>
+            <div>This is mainpage information</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+            <div>-</div>
+        </div>
     </div>
-  );
 }
 
 export default App;
